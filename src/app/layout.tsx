ac6811ee5cbb98e5ec3db5b304f6cfaa81191232
Renato Rodrigues <renato072002@gmail.com>
2023-12-28 19:13:51 -0300

    final

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Head from 'next/head'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -16,7 +17,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-br">
+      <Head>
+      <link rel="icon" href="/logo.jpg" sizes="any" />
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   )
 }
+
