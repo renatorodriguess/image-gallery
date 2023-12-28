eccd5aafea4ed4fd97e9bdbbc2fa4a2c9aca5165
Renato Rodrigues <renato072002@gmail.com>
2023-12-28 16:23:06 -0300

    error-commit

--- src/components/Cards/ImageOverlayCard.tsx
+++ src/components/Cards/ImageOverlayCard.tsx
@@ -5,13 +5,16 @@ import { twMerge } from "tailwind-merge";
 
 interface ImageCard extends Partial<ImageGallery>{
   className?: string;
-  onClick?: (id:string | number) => void;
+  onClick?: (id: string | number) => void;
 }
 
-const ImageOverlayCard = ({slug, className=""}: ImageCard) => {
+const ImageOverlayCard = ({ slug, className = "" }: ImageCard) => {
+  // Verifique se slug é uma string válida ou forneça um valor padrão
+  const imageSrc = slug || "caminho_do_valor_padrao.jpg";
+
   return (
     <div className={twMerge("rounded-lg overflow-hidden border border-gray-300 group flex items-center justify-center h-full", className)}>
-        <Image src={slug} alt={slug} className="block h-full w-full object-cover"/>
+      <Image src={imageSrc} alt={imageSrc} className="block h-full w-full object-cover" />
     </div>
   )
 }
